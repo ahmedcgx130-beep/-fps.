e4d4f92f623c96860e716647b8c4686342b2d7e6
ahmedcgx130-beep <ahmedcgx130@gmail.com>
2026-07-11 17:21:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: './',
-  plugins: [react()],
+  base: '/-fps/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
